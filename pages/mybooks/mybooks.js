6c6b2376c272b6295067b40114134487c9261eee
tww <497159777@qq.com>
2019-10-17 23:55:59 +0800

    Initial Commit

---
+++ pages/mybooks/mybooks.js
@@ -0,0 +1,74 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bookList: [
+      { ISBN13: "9789889955915", image: "/images/book-icon.png", title: "数据库挖掘", author: "斯塔夫", price: "88", publisher: "华理出版社", qty: "1" },
+      { ISBN13: "9789889955915", image: "/images/book-icon.png", title: "数据库挖掘", author: "斯塔夫", price: "88", publisher: "华理出版社", qty: "1" },
+      { ISBN13: "9789889955915", image: "/images/book-icon.png", title: "数据库挖掘", author: "斯塔夫", price: "88", publisher: "华理出版社", qty: "1" },
+      { ISBN13: "9789889955915", image: "/images/book-icon.png", title: "数据库挖掘", author: "斯塔夫", price: "88", publisher: "华理出版社", qty: "1" },
+    ]
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
