9989d037fd7b8d3abb282513714d274e094a47fd
ncdenaozi <ncdenaozi@outlook.com>
2019-11-20 11:30:46 +0800

    111
    111

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+				if (res.code) {
+					//发起网络请求
+					wx.request({
+						url: 'http://localhost:8080/wechat/servlet/LoginServlet',
+						data: {
+							code: res.code
+						},
+						method: 'GET',
+						header: {
+							'Content-Type': 'application/json',
+						}
+					})
+				} else {
+					console.log('登录失败！' + res.errMsg)
+				}
       }
     })
     // 获取用户信息
